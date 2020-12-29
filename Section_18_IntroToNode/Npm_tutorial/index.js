8f1a0a06490e63b0eeb3769b99fa745c586b9f44
Meerxn <56094406+Fardeen-Meeran@users.noreply.github.com>
2020-12-29 22:42:18 +0400

    Section 18 Completed

---
+++ Section_18_IntroToNode/Npm_tutorial/index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var x = superheroes.all
+console.log("hello World")
+console.log(x)
+var y = x[1]
+console.log(y)
+console.log(supervillains.random())
