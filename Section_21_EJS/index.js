fa3db3c59f9607b3b2e6021bf9f803f49e943b72
Meerxn <fardeenfmeeran@gmail.com>
2021-01-15 22:10:30 +0400

    ejs + TODOLIST APP

--- Section_21_EJS/index.js
+++ Section_21_EJS/index.js
@@ -9,18 +9,29 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+//app.use(express.static(__dirname +"/"));
+
 app.set('view engine', 'ejs');
 //app.use('view engine', 'ejs');
 
-
+var items = []; // scope of variable can be covered in both
+// we still have variable dec in our post request
 //We need templates to avoid sending html file for each situation
 app.get("/",(req,res)=>{
   
         
     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; // my version of doing day challenge 
+    var options = {
+        weekday: 'long',
+        day: "numeric",
+        month: 'long'
+
+    };
     
     var today = new Date();
-    var day = "";
+    var day = today.toLocaleDateString("en-US",options);
 
 //Day challenge used array and getDay() acts as an index
 
@@ -28,7 +39,7 @@ app.get("/",(req,res)=>{
 // We could have used swirdh statements but it did not seem like the move for me oops
 
 // This still WORKS
-    day = days[Number(today.getDay())];
+    //day = days[Number(today.getDay())];
 
 
     // if (today.getDay() === 6 || today.getDay === 0){
@@ -39,7 +50,8 @@ app.get("/",(req,res)=>{
     //     day = "weekday boi"
         
     // }
-    res.render('list', {type: day});
+
+    res.render('list', {type: day,newListItems:items});
     
 
 // We gonna use  EJS templating. As we cannot make html mages for very condition (to much work) hence we use a template
@@ -64,9 +76,16 @@ app.get("/",(req,res)=>{
     // }
    
 
-})
-
-app.listen(4068,()=>{
-    console.log("server is on");
 });
 
+app.post("/" ,(req,res)=>{
+ var item = req.body.newItem;
+ items.push(item); // doing this would add it side by side ( add with new tags ??)
+
+res.redirect("/");
+
+})
+
+app.listen(process.env.PORT|| 3000,()=>{
+    console.log("running");
+})
