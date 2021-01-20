d758a0115147fc5435a600e0ddd4f44e3124b643
Meerxn <fardeenfmeeran@gmail.com>
2021-01-20 17:26:33 +0400

    Intro to sql

---
+++ Section25_SQL/Intro.sql
@@ -0,0 +1,12 @@
+/* Intro to SQL Commands such as CREATE TABLE and INSERT DATA*/
+
+
+
+
+-- Create Table
+CREATE TABLE prod (
+    ID INT NOT NULL,
+    name STRING,
+    price MONEY,
+    PRIMARY KEY (ID)
+);
